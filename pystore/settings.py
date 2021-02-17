8113e5fcb98733dd01e3d0c87859391a7801440c
Fabio Ruicci <fcgr.fcgr@gmail.com>
2021-02-17 00:58:28 -0300

    aula 3 ok

--- pystore/settings.py
+++ pystore/settings.py
@@ -41,11 +41,14 @@ INSTALLED_APPS = [
     # apps de terceiros
     "debug_toolbar",
     "widget_tweaks",
+    "crispy_forms",
+    "localflavor",
     # meus apps
     "users.apps.UsersConfig",
     "pages.apps.PagesConfig",
     "products.apps.ProductsConfig",
     "cart.apps.CartConfig",
+    "orders.apps.OrdersConfig",
 ]
 
 MIDDLEWARE = [
@@ -159,3 +162,8 @@ INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
 
 CART_SESSION_ID = "cart"
 CART_ITEM_MAX_QUANTITY = 20
+
+
+# Crispy
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
